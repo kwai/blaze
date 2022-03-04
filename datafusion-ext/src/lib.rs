cf53a1e2a959aa0c2163345e367d27adcdbc398b
Zhang Li <richselian@gmail.com>
2022-03-04 11:51:55 +0800

    replace thread local job id with dashmap (#44)
    Co-authored-by: zhangli20 <zhangli20@kuaishou.com>

--- datafusion-ext/src/lib.rs
+++ datafusion-ext/src/lib.rs
@@ -1,8 +1,11 @@
-use std::cell::RefCell;
 use std::sync::Arc;
 
+use dashmap::DashMap;
 use datafusion::datasource::object_store::ObjectStoreRegistry;
+use jni::JNIEnv;
+
 use hdfs_object_store::HDFSSingleFileObjectStore;
+use jni_bridge::JavaClasses;
 
 pub mod hdfs_object_store; // note: can be changed to priv once plan transforming is removed
 pub mod jni_bridge;
@@ -20,25 +23,29 @@ lazy_static::lazy_static! {
         osr.register_store("viewfs".to_owned(), hdfs_object_store.clone());
         osr
     };
+
+    static ref JENV_JOB_IDS: Arc<DashMap<usize, String>> = Arc::default();
 }
 
 pub fn global_object_store_registry() -> &'static ObjectStoreRegistry {
     &OBJECT_STORE_REGISTRY
 }
 
-// set_job_id/get_job_id should only be used in main thread
-thread_local! {
-    static JOB_ID: RefCell<String> = RefCell::default()
-}
-
 pub fn set_job_id(job_id: &str) {
-    JOB_ID.with(|thread_local_job_id| {
-        let mut job_id_ref = thread_local_job_id.borrow_mut();
-        job_id_ref.clear();
-        job_id_ref.push_str(job_id);
-    });
+    let env_addr = unsafe {
+        // safety: transmute to raw pointer addr, only used as jobid map key
+        std::mem::transmute::<JNIEnv, usize>(JavaClasses::get_thread_jnienv())
+    };
+    JENV_JOB_IDS.insert(env_addr, job_id.to_owned());
 }
 
 pub fn get_job_id() -> String {
-    JOB_ID.with(|thread_local_job_id| thread_local_job_id.borrow().clone())
+    let env_addr = unsafe {
+        // safety: transmute to raw pointer addr, only used as jobid map key
+        std::mem::transmute::<JNIEnv, usize>(JavaClasses::get_thread_jnienv())
+    };
+    JENV_JOB_IDS
+        .get(&env_addr)
+        .expect("job id not set in current thread")
+        .clone()
 }
