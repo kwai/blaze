21fd5d74d90febfea04b37af0d944c19dc49e199
zhangli20 <zhangli20@kuaishou.com>
2022-04-24 12:01:32 +0800

    prevent wrapping HashAggregateExec with ConvertToUnsafeRow

--- spark-extension/src/main/scala/org/apache/spark/sql/blaze/BlazeSparkSessionExtension.scala
+++ spark-extension/src/main/scala/org/apache/spark/sql/blaze/BlazeSparkSessionExtension.scala
@@ -97,7 +97,7 @@ case class BlazeQueryStagePrepOverrides(sparkSession: SparkSession)
       case exec: SortMergeJoinExec if enableSmj => tryConvert(exec, convertSortMergeJoinExec)
       case exec @ (
             _: SortExec | _: CollectLimitExec | _: BroadcastExchangeExec | _: SortMergeJoinExec |
-            _: WindowExec | _: HashAggregateExec
+            _: WindowExec
           ) =>
         log.info(s"Ignore unsupported exec: ${exec.simpleStringWithNodeId()}")
         exec.mapChildren(child => convertToUnsafeRow(addWholeStageWrapper(child)))
