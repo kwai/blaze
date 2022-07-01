fe7e7462f433e416aceabc09f50453a3a3f06640
zhangli20 <zhangli20@kuaishou.com>
2022-07-01 19:24:13 +0800

    add staging buffers to reduce memory usage

--- native-engine/datafusion-ext/src/shuffle_writer_exec.rs
+++ native-engine/datafusion-ext/src/shuffle_writer_exec.rs
@@ -65,151 +65,143 @@ use crate::util::ipc::write_ipc_compressed;
 struct PartitionBuffer {
     schema: SchemaRef,
     frozen: Vec<u8>,
+    staging: Vec<RecordBatch>,
     active: Vec<Box<dyn ArrayBuilder>>,
-    active_num_rows: usize,
+    num_active_rows: usize,
+    num_staging_rows: usize,
     batch_size: usize,
+    staging_size: usize,
 }
 
 impl PartitionBuffer {
     fn new(schema: SchemaRef, batch_size: usize) -> Self {
-        let array_builders = new_array_builders(&schema, batch_size);
+        let staging_size = (batch_size as f64).sqrt() as usize;
+        let array_builders = new_array_builders(&schema, staging_size);
         Self {
             schema,
             frozen: vec![],
+            staging: vec![],
             active: array_builders,
-            active_num_rows: 0,
+            num_active_rows: 0,
+            num_staging_rows: 0,
             batch_size,
+            staging_size,
         }
     }
 
-    fn append_rows(
-        &mut self,
-        appenders: &Appenders,
-        columns: &[ArrayRef],
-        indices: &[usize],
-    ) -> Result<isize> {
-        // returns estimated memory diff
-        self.active.iter_mut().zip(columns).enumerate().for_each(
-            |(i, (builder, column))| {
-                appenders
-                    .append_column(i, builder, column, indices)
-                    .unwrap();
-            },
-        );
-        self.active_num_rows += indices.len();
-        if self.active_num_rows >= self.batch_size {
-            return self.flush();
+    fn append_rows(&mut self, columns: &[ArrayRef], indices: &[usize]) -> Result<isize> {
+        self.active
+            .iter_mut()
+            .zip(columns)
+            .for_each(|(builder, column)| {
+                append_columns(builder, column, indices, column.data_type());
+            });
+        self.num_active_rows += indices.len();
+        if self.num_active_rows >= self.staging_size {
+            return self.flush_to_staging();
         }
         Ok(0) // assume no memory growing if not flushed
     }
 
-    fn flush(&mut self) -> Result<isize> {
-        // returns estimated memory diff
-        if self.active_num_rows == 0 {
+    /// flush active data into one staging batch
+    fn flush_to_staging(&mut self) -> Result<isize> {
+        if self.num_active_rows == 0 {
             return Ok(0);
         }
+        let mut mem_diff = 0isize;
+
+        // active -> staging
+        let active = std::mem::replace(
+            &mut self.active,
+            new_array_builders(&self.schema, self.staging_size),
+        );
+        self.num_staging_rows += self.num_active_rows;
+        self.num_active_rows = 0;
+
+        let staging_batch = make_batch(self.schema.clone(), active)?;
+        mem_diff += batch_byte_size(&staging_batch) as isize;
+        self.staging.push(staging_batch);
+
+        // staging -> frozen
+        if self.num_staging_rows >= self.batch_size {
+            mem_diff += self.flush()?;
+        }
+        Ok(mem_diff)
+    }
+
+    /// flush all active and staging data into frozen bytes
+    fn flush(&mut self) -> Result<isize> {
+        let mut mem_diff = 0isize;
+
+        if self.num_active_rows > 0 {
+            mem_diff += self.flush_to_staging()?;
+        }
+        if self.staging.is_empty() {
+            return Ok(mem_diff);
+        }
+
+        let frozen_batch = RecordBatch::concat(&self.schema, &self.staging)?;
+        mem_diff -= self
+            .staging
+            .iter()
+            .map(|batch| batch_byte_size(batch) as isize)
+            .sum::<isize>();
+        self.staging.clear();
+        self.num_staging_rows = 0;
+
         let frozen_capacity_old = self.frozen.capacity();
         let mut cursor = Cursor::new(&mut self.frozen);
         cursor.seek(SeekFrom::End(0))?;
+        write_ipc_compressed(&frozen_batch, &mut cursor)?;
 
-        let active = std::mem::replace(
-            &mut self.active,
-            new_array_builders(&self.schema, self.batch_size),
-        );
-        self.active_num_rows = 0;
-
-        let batch = make_batch(self.schema.clone(), active)?;
-        let batch_bytes_size = batch_byte_size(&batch);
-        write_ipc_compressed(&batch, &mut cursor)?;
-
-        // return the increased amount of memory used
-        Ok((self.frozen.capacity() - frozen_capacity_old) as isize
-            - batch_bytes_size as isize)
+        mem_diff += (self.frozen.capacity() - frozen_capacity_old) as isize;
+        Ok(mem_diff)
     }
 }
 
-type AppendFn =
-    &'static dyn Fn(&mut Box<dyn ArrayBuilder>, &Arc<dyn Array>, &[usize]) -> Result<()>;
-
-#[derive(Clone)]
-struct Appenders(Vec<AppendFn>);
-
-unsafe impl Send for Appenders {}
-unsafe impl Sync for Appenders {}
-
-impl Appenders {
-    fn new(schema: &SchemaRef) -> Self {
-        macro_rules! define_appender {
-            ($arrowty:ident) => {{
-                paste::paste! {
-                    fn append(
-                        t: &mut Box<dyn ArrayBuilder>,
-                        f: &Arc<dyn Array>,
-                        indices: &[usize],
-                    ) -> Result<()> {
-                        type B = [< $arrowty Builder >];
-                        type A = [< $arrowty Array >];
-                        let t = t.as_any_mut().downcast_mut::<B>().unwrap();
-                        let f = f.as_any().downcast_ref::<A>().unwrap();
-                        for &i in indices {
-                            if f.is_valid(i) {
-                                t.append_value(f.value(i))?;
-                            } else {
-                                t.append_null()?;
-                            }
-                        }
-                        Ok(())
-                    }
-                    &append as AppendFn
+fn append_columns(
+    to: &mut Box<dyn ArrayBuilder>,
+    from: &Arc<dyn Array>,
+    indices: &[usize],
+    data_type: &DataType,
+) {
+    macro_rules! append {
+        ($arrowty:ident) => {{
+            type B = paste::paste! {[< $arrowty Builder >]};
+            type A = paste::paste! {[< $arrowty Array >]};
+            let t = to.as_any_mut().downcast_mut::<B>().unwrap();
+            let f = from.as_any().downcast_ref::<A>().unwrap();
+            for &i in indices {
+                if f.is_valid(i) {
+                    t.append_value(f.value(i)).unwrap();
+                } else {
+                    t.append_null().unwrap();
                 }
-            }};
-        }
-
-        Self(
-            schema
-                .fields()
-                .iter()
-                .map(|field| match field.data_type() {
-                    DataType::Boolean => define_appender!(Boolean),
-                    DataType::Int8 => define_appender!(Int8),
-                    DataType::Int16 => define_appender!(Int16),
-                    DataType::Int32 => define_appender!(Int32),
-                    DataType::Int64 => define_appender!(Int64),
-                    DataType::UInt8 => define_appender!(UInt8),
-                    DataType::UInt16 => define_appender!(UInt16),
-                    DataType::UInt32 => define_appender!(UInt32),
-                    DataType::UInt64 => define_appender!(UInt64),
-                    DataType::Float32 => define_appender!(Float32),
-                    DataType::Float64 => define_appender!(Float64),
-                    DataType::Date32 => define_appender!(Date32),
-                    DataType::Date64 => define_appender!(Date64),
-                    DataType::Time32(TimeUnit::Second) => define_appender!(Time32Second),
-                    DataType::Time32(TimeUnit::Millisecond) => {
-                        define_appender!(Time32Millisecond)
-                    }
-                    DataType::Time64(TimeUnit::Microsecond) => {
-                        define_appender!(Time64Microsecond)
-                    }
-                    DataType::Time64(TimeUnit::Nanosecond) => {
-                        define_appender!(Time64Nanosecond)
-                    }
-                    DataType::Utf8 => define_appender!(String),
-                    DataType::LargeUtf8 => define_appender!(LargeString),
-                    DataType::Decimal(_, _) => define_appender!(Decimal),
-                    _ => unimplemented!("unsupported data types in shuffle write"),
-                })
-                .collect(),
-        )
+            }
+        }};
     }
-
-    fn append_column(
-        &self,
-        column_index: usize,
-        to: &mut Box<dyn ArrayBuilder>,
-        from: &Arc<dyn Array>,
-        indices: &[usize],
-    ) -> Result<()> {
-        (self.0[column_index])(to, from, indices)
+    match data_type {
+        DataType::Boolean => append!(Boolean),
+        DataType::Int8 => append!(Int8),
+        DataType::Int16 => append!(Int16),
+        DataType::Int32 => append!(Int32),
+        DataType::Int64 => append!(Int64),
+        DataType::UInt8 => append!(UInt8),
+        DataType::UInt16 => append!(UInt16),
+        DataType::UInt32 => append!(UInt32),
+        DataType::UInt64 => append!(UInt64),
+        DataType::Float32 => append!(Float32),
+        DataType::Float64 => append!(Float64),
+        DataType::Date32 => append!(Date32),
+        DataType::Date64 => append!(Date64),
+        DataType::Time32(TimeUnit::Second) => append!(Time32Second),
+        DataType::Time32(TimeUnit::Millisecond) => append!(Time32Millisecond),
+        DataType::Time64(TimeUnit::Microsecond) => append!(Time64Microsecond),
+        DataType::Time64(TimeUnit::Nanosecond) => append!(Time64Nanosecond),
+        DataType::Utf8 => append!(String),
+        DataType::LargeUtf8 => append!(LargeString),
+        DataType::Decimal(_, _) => append!(Decimal),
+        _ => unimplemented!("data type not supported in shuffle write"),
     }
 }
 
@@ -222,7 +214,6 @@ struct ShuffleRepartitioner {
     id: MemoryConsumerId,
     output_data_file: String,
     output_index_file: String,
-    appenders: Appenders,
     schema: SchemaRef,
     buffered_partitions: Mutex<Vec<PartitionBuffer>>,
     spills: Mutex<Vec<SpillInfo>>,
@@ -251,7 +242,6 @@ impl ShuffleRepartitioner {
             id: MemoryConsumerId::new(partition_id),
             output_data_file,
             output_index_file,
-            appenders: Appenders::new(&schema),
             schema: schema.clone(),
             buffered_partitions: Mutex::new(
                 (0..num_output_partitions)
@@ -276,12 +266,6 @@ impl ShuffleRepartitioner {
         // NOTE: in shuffle writer exec, the output_rows metrics represents the
         // number of rows those are written to output data file.
         self.metrics.record_output(input.num_rows());
-
-        // a batch is inserted into active builder of each partition, so the
-        // uncompressed memory size of the batch must be consumed first
-        let batch_mem_size = batch_byte_size(&input);
-        self.try_grow(batch_mem_size).await?;
-        self.metrics.mem_used().add(batch_mem_size);
 
         let num_output_partitions = self.num_output_partitions;
         match &self.partitioning {
@@ -337,16 +321,21 @@ impl ShuffleRepartitioner {
                     let output = &mut buffered_partitions[partition_id];
 
                     let mem_diff = output.append_rows(
-                        &self.appenders,
                         input.columns(),
                         &shuffled_partition_ids[start..end],
                     )?;
+                    std::mem::drop(buffered_partitions);
 
+                    if mem_diff > 0 {
+                        let mem_increase = mem_diff as usize;
+                        self.try_grow(mem_increase).await?;
+                        self.metrics.mem_used().add(mem_increase);
+                    }
                     if mem_diff < 0 {
                         let mem_used = self.metrics.mem_used().value();
-                        let to_shrink = mem_used.min(-mem_diff as usize);
-                        self.shrink(to_shrink);
-                        self.metrics.mem_used().set(mem_used - to_shrink);
+                        let mem_decrease = mem_used.min(-mem_diff as usize);
+                        self.shrink(mem_decrease);
+                        self.metrics.mem_used().set(mem_used - mem_decrease);
                     }
                 }
             }
