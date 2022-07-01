98398432079e655421e258b60a80c6a35abdd715
Yijie Shen <henry.yijieshen@gmail.com>
2022-07-01 10:26:17 +0800

    fix build script with debug mode

--- build-native.sh
+++ build-native.sh
@@ -11,10 +11,15 @@ if [[ "$rt" != 0 ]]; then
   exit $rt
 fi
 rm -rf `pwd`/../lib/*
+
+[[ $profile == "dev" ]] \
+  && libdir="debug" \
+  || libdir="$profile"
+
 if [ "$(uname)" == "Darwin" ]; then
-  mkdir -p `pwd`/../lib/ && cp `pwd`/../target/$profile/*.dylib `pwd`/../lib/
+  mkdir -p `pwd`/../lib/ && cp `pwd`/../target/$libdir/*.dylib `pwd`/../lib/
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  mkdir -p `pwd`/../lib/ && cp `pwd`/../target/$profile/*.so `pwd`/../lib/
+  mkdir -p `pwd`/../lib/ && cp `pwd`/../target/$libdir/*.so `pwd`/../lib/
 else
     echo "Unsupported platform $(uname)"
     exit 1
