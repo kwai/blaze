2992bf2ca5b464de0fed66414286d05702dfaf1c
zhangli20 <zhangli20@kuaishou.com>
2022-11-07 00:23:04 +0800

    refactor: rename package plan-serde to blaze-serde

--- native-engine/blaze-serde/build.rs
+++ native-engine/blaze-serde/build.rs
@@ -16,8 +16,8 @@ fn main() -> Result<(), String> {
     // for use in docker build where file changes can be wonky
     println!("cargo:rerun-if-env-changed=FORCE_REBUILD");
 
-    println!("cargo:rerun-if-changed=proto/plan.proto");
+    println!("cargo:rerun-if-changed=proto/blaze.proto");
     tonic_build::configure()
-        .compile(&["proto/plan.proto"], &["proto"])
+        .compile(&["proto/blaze.proto"], &["proto"])
         .map_err(|e| format!("protobuf compilation failed: {}", e))
 }
