e30bc1297a9d791acd2e0e661b0f1adde45b6651
Zhang Li <richselian@gmail.com>
2024-01-30 18:24:03 +0800

    use unchecked index in LoserTree to get slightly performance improvement. (#378)
    Co-authored-by: zhangli20 <zhangli20@kuaishou.com>

--- native-engine/datafusion-ext-commons/src/loser_tree.rs
+++ native-engine/datafusion-ext-commons/src/loser_tree.rs
@@ -14,22 +14,29 @@
 
 use std::ops::{Deref, DerefMut};
 
+use unchecked_index::{unchecked_index, UncheckedIndex};
+
 pub trait ComparableForLoserTree {
     fn lt(&self, other: &Self) -> bool;
 }
 
 /// An implementation of the tournament loser tree data structure.
 pub struct LoserTree<T> {
-    nodes: Vec<usize>,
-    values: Vec<T>,
+    losers: UncheckedIndex<Vec<usize>>,
+    values: UncheckedIndex<Vec<T>>,
 }
 
 #[allow(clippy::len_without_is_empty)]
 impl<T: ComparableForLoserTree> LoserTree<T> {
     pub fn new(values: Vec<T>) -> Self {
-        let mut tree = Self {
-            nodes: vec![],
-            values,
+        let mut tree = unsafe {
+            // safety:
+            // this component is performance critical,  use unchecked index
+            // to avoid boundary checking.
+            Self {
+                losers: unchecked_index(vec![]),
+                values: unchecked_index(values),
+            }
         };
         tree.init_tree();
         tree
@@ -55,37 +62,36 @@ impl<T: ComparableForLoserTree> LoserTree<T> {
     }
 
     fn init_tree(&mut self) {
-        self.nodes = vec![usize::MAX; self.values.len()];
-
+        self.losers.resize(self.values.len(), usize::MAX);
         for i in 0..self.values.len() {
             let mut winner = i;
             let mut cmp_node = (self.values.len() + i) / 2;
-            while cmp_node != 0 && self.nodes[cmp_node] != usize::MAX {
-                let challenger = self.nodes[cmp_node];
+            while cmp_node != 0 && self.losers[cmp_node] != usize::MAX {
+                let challenger = self.losers[cmp_node];
                 if self.values[challenger].lt(&self.values[winner]) {
-                    self.nodes[cmp_node] = winner;
+                    self.losers[cmp_node] = winner;
                     winner = challenger;
                 } else {
-                    self.nodes[cmp_node] = challenger;
+                    self.losers[cmp_node] = challenger;
                 }
                 cmp_node /= 2;
             }
-            self.nodes[cmp_node] = winner;
+            self.losers[cmp_node] = winner;
         }
     }
 
     fn adjust_tree(&mut self) {
-        let mut winner = self.nodes[0];
+        let mut winner = self.losers[0];
         let mut cmp_node = (self.values.len() + winner) / 2;
         while cmp_node != 0 {
-            let challenger = self.nodes[cmp_node];
+            let challenger = self.losers[cmp_node];
             if self.values[challenger].lt(&self.values[winner]) {
-                self.nodes[cmp_node] = winner;
+                self.losers[cmp_node] = winner;
                 winner = challenger;
             }
             cmp_node /= 2;
         }
-        self.nodes[0] = winner;
+        self.losers[0] = winner;
     }
 }
 
@@ -100,14 +106,14 @@ impl<T: ComparableForLoserTree> Deref for LoserTreePeekMut<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        &self.tree.values[self.tree.nodes[0]]
+        &self.tree.values[self.tree.losers[0]]
     }
 }
 
 impl<T: ComparableForLoserTree> DerefMut for LoserTreePeekMut<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.dirty = true;
-        &mut self.tree.values[self.tree.nodes[0]]
+        &mut self.tree.values[self.tree.losers[0]]
     }
 }
 
@@ -115,6 +121,81 @@ impl<T: ComparableForLoserTree> Drop for LoserTreePeekMut<'_, T> {
     fn drop(&mut self) {
         if self.dirty {
             self.tree.adjust_tree();
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use itertools::Itertools;
+    use rand::Rng;
+
+    use crate::loser_tree::{ComparableForLoserTree, LoserTree};
+
+    #[test]
+    fn fuzztest() {
+        for _ in 0..10 {
+            let num_nodes = rand::thread_rng().gen_range(1..=999);
+            let mut nodes = vec![];
+            for _ in 0..num_nodes {
+                let node_len = rand::thread_rng().gen_range(1..=999);
+                let mut node = vec![];
+                for _ in 0..node_len {
+                    node.push(rand::thread_rng().gen_range(1000..=9999));
+                }
+                nodes.push(node);
+            }
+
+            // expected
+            let expected = nodes
+                .clone()
+                .into_iter()
+                .flatten()
+                .sorted_unstable()
+                .collect_vec();
+
+            // actual
+            struct Cursor {
+                row_idx: usize,
+                values: Vec<u64>,
+            }
+            impl ComparableForLoserTree for Cursor {
+                fn lt(&self, other: &Self) -> bool {
+                    match (
+                        self.values.get(self.row_idx),
+                        other.values.get(other.row_idx),
+                    ) {
+                        (Some(v1), Some(v2)) => v1 < v2,
+                        (None, _) => false,
+                        (_, None) => true,
+                    }
+                }
+            }
+            let mut loser_tree = LoserTree::new(
+                nodes
+                    .into_iter()
+                    .map(|node| Cursor {
+                        row_idx: 0,
+                        values: node.into_iter().sorted_unstable().collect_vec(),
+                    })
+                    .collect_vec(),
+            );
+
+            let mut actual = vec![];
+            loop {
+                let mut min = loser_tree.peek_mut();
+                if let Some(v) = min.values.get(min.row_idx) {
+                    actual.push(*v);
+                    min.row_idx += 1;
+                } else {
+                    break;
+                }
+            }
+
+            for cursor in loser_tree.values() {
+                assert_eq!(cursor.row_idx, cursor.values.len());
+            }
+            assert_eq!(actual, expected);
         }
     }
 }
