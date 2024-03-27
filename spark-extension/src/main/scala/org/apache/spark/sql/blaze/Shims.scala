afd65e78c916bb7e4dd7e20f03c06e751639b7d3
lihao29 <lihao29@kuaishou.com>
2024-03-27 15:11:52 +0800

    fix

--- spark-extension/src/main/scala/org/apache/spark/sql/blaze/Shims.scala
+++ spark-extension/src/main/scala/org/apache/spark/sql/blaze/Shims.scala
@@ -19,6 +19,7 @@ import java.io.File
 
 import org.apache.spark.ShuffleDependency
 import org.apache.spark.TaskContext
+import org.apache.spark.SparkContext
 import org.blaze.{protobuf => pb}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.scheduler.MapStatus
