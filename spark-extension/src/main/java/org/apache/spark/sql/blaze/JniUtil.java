4e9d94851da948a2337991f5e20dfacfc65085ab
Zhang Li <richselian@gmail.com>
2024-09-24 20:09:24 +0800

    use hadoop fs positioned-reading (#582)
    Co-authored-by: zhangli20 <zhangli20@kuaishou.com>

--- spark-extension/src/main/java/org/apache/spark/sql/blaze/JniUtil.java
+++ spark-extension/src/main/java/org/apache/spark/sql/blaze/JniUtil.java
@@ -21,35 +21,69 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
+
+import org.apache.hadoop.fs.ByteBufferPositionedReadable;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 
+@SuppressWarnings("unused")
 public class JniUtil {
+    static ReadImpl readImpl;
+    static ReadImpl readFallback = new ReadFailback();
+    static {
+        try {
+            readImpl = new ReadUsingByteBufferPositionedReadable();
+        } catch (NoClassDefFoundError e) {
+            readImpl = readFallback;
+        }
+    }
     public static void readFullyFromFSDataInputStream(FSDataInputStream in, long pos, ByteBuffer buf)
             throws IOException {
-
-        synchronized (in) {
-            if (pos != in.getPos()) {
-                in.seek(pos);
-            }
-            ReadableByteChannel channel = Channels.newChannel(in);
-
-            while (buf.hasRemaining()) {
-                if (channel.read(buf) == -1) {
-                    throw new EOFException("readFullyFromFSDataInputStream() got unexpected EOF");
-                }
-            }
+        try {
+            readFallback.read(in, pos, buf);
+        } catch (UnsupportedOperationException e) {
+            readImpl.read(in, pos, buf);
         }
     }
 
     public static void writeFullyToFSDataOutputStream(FSDataOutputStream out, ByteBuffer buf) throws IOException {
-
         synchronized (out) {
             WritableByteChannel channel = Channels.newChannel(out);
 
             while (buf.hasRemaining()) {
                 if (channel.write(buf) == -1) {
                     throw new EOFException("writeFullyToFSDataOutputStream() got unexpected EOF");
+                }
+            }
+        }
+    }
+
+    private interface ReadImpl {
+        void read(FSDataInputStream in, long pos, ByteBuffer buf) throws IOException;
+    }
+
+    private static class ReadUsingByteBufferPositionedReadable implements ReadImpl {
+        @Override
+        public void read(FSDataInputStream in, long pos, ByteBuffer buf) throws IOException {
+            while (buf.hasRemaining()) {
+                if (((ByteBufferPositionedReadable) in).read(pos, buf) == -1) {
+                    throw new EOFException("readFullyFromFSDataInputStream() got unexpected EOF");
+                }
+                pos += buf.position();
+            }
+        }
+    }
+
+    private static class ReadFailback implements ReadImpl {
+        @Override
+        public void read(FSDataInputStream in, long pos, ByteBuffer buf) throws IOException {
+            synchronized (in) {
+                in.seek(pos);
+                while (buf.hasRemaining()) {
+                    ReadableByteChannel channel = Channels.newChannel(in);
+                    if (channel.read(buf) == -1) {
+                        throw new EOFException("readFullyFromFSDataInputStream() got unexpected EOF");
+                    }
                 }
             }
         }
